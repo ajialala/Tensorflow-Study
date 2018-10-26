eda2f34ade688894b66f49a3a83be7f6f8824c25
ajialala <zhangjloo1@163.com>
2018-10-26 21:58:30 +0800

    改正monitor.py中的bug

--- MNIST数字识别_TensorBoard可视化版/README.md
+++ MNIST数字识别_TensorBoard可视化版/README.md
@@ -2,3 +2,4 @@
 - mnist_train.py文件中，将完成类似功能的计算放到了由tf.name_scope函数生成上下文管理器中。这样TensorBoard可以将这些节点有效的合并，从而突出神经网络的整体结构。
 - 因为在mnist_inferenece.py程序中已经使用了tf.variable_scope来管理变量的命名空间，所以这里不需要再做调整。
 - 运行完这段代码之后在终端运行 tensorboard --logdir=log/。然后在浏览器输入localhost:6006打开。
+- monitor.py是一个可以单独运行的文件，含有监控指标可视化功能。
--- MNIST数字识别_TensorBoard可视化版/monitor.py
+++ MNIST数字识别_TensorBoard可视化版/monitor.py
@@ -7,6 +7,7 @@ SUMMARY_DIR = 'log/'
 BATCH_SIZE = 100
 TRAINING_STEPS = 30000
 
+
 # 生成变量监控信息并定义变量监控信息日志的操作。其中var给出了需要记录的张量，
 # name给出了在可视化结果中显示的图表名称，这个名称一般与变量名一致。
 def variable_summaries(var, name):
@@ -25,81 +26,84 @@ def variable_summaries(var, name):
         stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
         tf.summary.scalar('stddev/' + name, stddev)
 
-    # 生成一层全连接层神经网络。
-    def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
-        # 将同一层神经网络放在一个统一的命名空间下。
-        with tf.name_scope(layer_name):
-            # 声明神经网络边上的权重，并调用生成权重监控信息日志的函数
-            with tf.name_scope('weights'):
-                weights = tf.Variable(tf.truncated_normal([input_dim, output_dim], stddev=0.1))
-                variable_summaries(weights, layer_name + 'weights')
 
-            # 声明神经网络的偏置项，并调用生成偏置项监控信息日志的函数
-            with tf.name_scope('biases'):
-                biases = tf.Variable(tf.constant(0.0, shape=[output_dim]))
-                variable_summaries(biases, layer_name + 'biases')
+# 生成一层全连接层神经网络。
+def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
+    # 将同一层神经网络放在一个统一的命名空间下。
+    with tf.name_scope(layer_name):
+        # 声明神经网络边上的权重，并调用生成权重监控信息日志的函数
+        with tf.name_scope('weights'):
+            weights = tf.Variable(tf.truncated_normal([input_dim, output_dim], stddev=0.1))
+            variable_summaries(weights, layer_name + 'weights')
 
-            with tf.name_scope('Wx_plus_b'):
-                preactivate = tf.matmul(input_tensor, weights) + biases
-                # 记录神经网络输出节点在经过激活函数之前的分布。
-                tf.summary.histogram(layer_name + '/preactivate', preactivate)
-            activations = act(preactivate, name='activation')
+        # 声明神经网络的偏置项，并调用生成偏置项监控信息日志的函数
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.constant(0.0, shape=[output_dim]))
+            variable_summaries(biases, layer_name + 'biases')
 
-            # 记录神经网络输出节点在经过激活函数之后的分布。对于layer1，因为使用RELU函数作为激活函数，所以所有小于0的值都被设为了0。
-            # 于是在激活后的layer1/activations图上所有的值都是大于0的。而对于layer2，因为没有使用激活函数，所以layer2/activations
-            # 和layer2/pre_activations一样。
-            tf.summary.histogram(layer_name + '/activations', activations)
-            return activations
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.matmul(input_tensor, weights) + biases
+            # 记录神经网络输出节点在经过激活函数之前的分布。
+            tf.summary.histogram(layer_name + '/preactivate', preactivate)
+        activations = act(preactivate, name='activation')
 
-    def main(_):
-        mnist = input_data.read_data_sets("mnist_data", one_hot=True)
-        # 定义输入
-        with tf.name_scope('input'):
-            x = tf.placeholder(tf.float32, [None, 784], name='x-input')
-            y_ = tf.placeholder(tf.float32, [None, 10], name='y-input')
+        # 记录神经网络输出节点在经过激活函数之后的分布。对于layer1，因为使用RELU函数作为激活函数，所以所有小于0的值都被设为了0。
+        # 于是在激活后的layer1/activations图上所有的值都是大于0的。而对于layer2，因为没有使用激活函数，所以layer2/activations
+        # 和layer2/pre_activations一样。
+        tf.summary.histogram(layer_name + '/activations', activations)
+        return activations
 
-        # 将输入向量还原成图片的像素矩阵，并通过tf.summary.image函数定义将当前的图片信息写入日志的操作。
-        with tf.name_scope('input_reshape'):
-            image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
-            tf.summary.image('input', image_shaped_input, 10)
 
-        hidden1 = nn_layer(x, 784, 500, 'layer1')
-        y = nn_layer(hidden1, 500, 10, 'layer2', act=tf.identity)
+def main(_):
+    mnist = input_data.read_data_sets("mnist_data", one_hot=True)
+    # 定义输入
+    with tf.name_scope('input'):
+        x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+        y_ = tf.placeholder(tf.float32, [None, 10], name='y-input')
 
-        # 计算交叉熵并定义生成交叉熵监控日志的操作。
-        with tf.name_scope('cross_entropy'):
-            cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=y_))
-            tf.summary.scalar('cross entropy', cross_entropy)
+    # 将输入向量还原成图片的像素矩阵，并通过tf.summary.image函数定义将当前的图片信息写入日志的操作。
+    with tf.name_scope('input_reshape'):
+        image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
+        tf.summary.image('input', image_shaped_input, 10)
 
-        with tf.name_scope('train'):
-            train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+    hidden1 = nn_layer(x, 784, 500, 'layer1')
+    y = nn_layer(hidden1, 500, 10, 'layer2', act=tf.identity)
 
-        # 计算模型在当前给定数据上的正确率，并定义生成正确率监控日志的操作。
+    # 计算交叉熵并定义生成交叉熵监控日志的操作。
+    with tf.name_scope('cross_entropy'):
+        cross_entropy = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1)))
+        tf.summary.scalar('cross entropy', cross_entropy)
+
+    with tf.name_scope('train'):
+        train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+
+    # 计算模型在当前给定数据上的正确率，并定义生成正确率监控日志的操作。
+    with tf.name_scope('accuracy'):
+        with tf.name_scope('correct_prediction'):
+            correct_prediction = tf.equal(tf.arg_max(y, 1), tf.arg_max(y_, 1))
         with tf.name_scope('accuracy'):
-            with tf.name_scope('correct_prediction'):
-                correct_prediction = tf.equal(tf.arg_max(y, 1), tf.arg_max(y_, 1))
-            with tf.name_scope('accuracy'):
-                accuracy = tf.reduce_mean(tf.cast(correct_prediction), tf.float32)
-            tf.summary.scalar('accuracy', accuracy)
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('accuracy', accuracy)
 
-        # 由于程序中定义的写日志操作比较多，一一调用sess.run执行比较麻烦，所有提供了ttf.summary.merge_all函数来整理所有的日志生成操作。
-        # 在Tensorflow程序执行的过程中只需要运行这个操作就可以将代码中定义的所有日志生成操作执行一次，从而将所有日志写入文件。
-        merged = tf.summary.merge_all()
+    # 由于程序中定义的写日志操作比较多，一一调用sess.run执行比较麻烦，所有提供了ttf.summary.merge_all函数来整理所有的日志生成操作。
+    # 在Tensorflow程序执行的过程中只需要运行这个操作就可以将代码中定义的所有日志生成操作执行一次，从而将所有日志写入文件。
+    merged = tf.summary.merge_all()
 
-        with tf.Session() as sess:
-            # 初始化写日志的writer，并将当前Tensorflow计算图写入日志。
-            summary_writer = tf.summary.FileWriter(SUMMARY_DIR, sess.graph)
-            tf.initialize_all_variables().run()
+    with tf.Session() as sess:
+        # 初始化写日志的writer，并将当前Tensorflow计算图写入日志。
+        summary_writer = tf.summary.FileWriter(SUMMARY_DIR, sess.graph)
+        tf.initialize_all_variables().run()
 
-            for i range(TRAINING_STEPS):
-                xs, ys = mnist.train.next_batch(BATCH_SIZE)
-                # 运行训练步骤以及所有的日志生成操作，得到这次运行的日志。
-                summary, _ = sess.run([merged, train_step], feed_dict={x: xs, y_: ys})
-                # 将所有日志写入文件，TensorBoard就可以拿到这次运行所对应的运行信息。
-                summary_writer.add_summary(summary, i)
+        for i in range(TRAINING_STEPS):
+            xs, ys = mnist.train.next_batch(BATCH_SIZE)
+            # 运行训练步骤以及所有的日志生成操作，得到这次运行的日志。
+            summary, _ = sess.run([merged, train_step], feed_dict={x: xs, y_: ys})
+            # 将所有日志写入文件，TensorBoard就可以拿到这次运行所对应的运行信息。
+            summary_writer.add_summary(summary, i)
 
-        summary_writer.close()
+    summary_writer.close()
 
-    if __name__ == '__main__':
-        tf.app.run()
 
+if __name__ == '__main__':
+    tf.app.run()
